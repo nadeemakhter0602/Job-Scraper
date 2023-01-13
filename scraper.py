dc24730afd46c2b75c7d9fc9ac7f70cb07785fe4
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-01-13 18:25:21 +0530

    create Selenium_Scraper class

---
+++ scraper.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import logging
+
+
+class Selenium_Scraper:
+    def __init__(self):
+        # configure log file
+        logging.basicConfig(filename="selenium_scraper.log",
+                            format='%(asctime)s %(message)s',
+                            filemode='w')
+        # create log object
+        self.logger = logging.getLogger()
+        # setting threshold
+        self.logger.setLevel(logging.INFO)
+
+    def initialize_webdriver(self):
+        # run chromium webdriver in headless mode
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_experimental_option('excludeSwitches', 
+                                               ['enable-logging'])
+        driver = webdriver.Chrome(options=chrome_options)
+        return driver
